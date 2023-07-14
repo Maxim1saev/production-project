20c3390c583fa3f358372cf50a26b93b0616b26f
Maxim1saev <37245038+Maxim1saev@users.noreply.github.com>
2023-07-15 02:12:46 +0300

    добавлены условия в CI сценарий

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,9 +11,7 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-               
-            </div>
+            <div className={cls.links} />
         </div>
     );
 };
